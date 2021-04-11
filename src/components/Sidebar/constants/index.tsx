5e45509d9c15c3684c268f821b60965e17b39e96
MarcosRene <renemoura789@gmail.com>
2021-04-11 18:35:17 -0300

    :sparkles: feat: create Sidebar component

---
+++ src/components/Sidebar/constants/index.tsx
@@ -0,0 +1,28 @@
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from 'react-icons/ri';
+
+export const GENERAL_MENU = {
+  Dashboard: {
+    path: '',
+    icon: RiDashboardLine,
+  },
+  Usuários: {
+    path: '',
+    icon: RiContactsLine,
+  },
+};
+
+export const AUTOMATION_MENU = {
+  Formulários: {
+    path: '',
+    icon: RiInputMethodLine,
+  },
+  Automação: {
+    path: '',
+    icon: RiGitMergeLine,
+  },
+};
