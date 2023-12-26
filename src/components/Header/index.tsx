b695bffe81011c22055445d21d434386362d44d0
Marcos Renê <renemoura789@gmail.com>
2023-12-26 17:53:51 -0300

    style: add responsiveness to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,17 +8,23 @@ import {
   HStack,
   Box,
   Avatar,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
+import { useBreakpointValue } from "@chakra-ui/react";
 
-import { RiSearchLine, RiNotificationLine, RiUserLine } from 'react-icons/ri';
+import { RiSearchLine, RiNotificationLine, RiUserLine } from "react-icons/ri";
 
 export const Header = () => {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Center>
       <Flex w="100%" maxW={1480} h="20" mt="4" px="6" align="center">
         <Heading
           as="h1"
-          fontSize="3xl"
+          fontSize={["2xl", "3xl"]}
           fontWeight="bold"
           letterSpacing="tight"
           w="64"
@@ -29,35 +35,37 @@ export const Header = () => {
           </Text>
         </Heading>
 
-        <Flex
-          as="label"
-          flex="1"
-          py="4"
-          px="8"
-          ml="6"
-          maxW={400}
-          alignSelf="center"
-          color="gray.200"
-          position="relative"
-          bg="gray.800"
-          borderRadius="full"
-        >
-          <Input
-            px="4"
-            mx="4"
-            variant="unstyled"
-            placeholder="Buscar na plataforma"
-            color="gray.50"
-            _placeholder={{ color: 'gray.400' }}
-          />
-          <Icon as={RiSearchLine} fontSize="20" />
-        </Flex>
+        {isWideVersion && (
+          <Flex
+            as="label"
+            flex="1"
+            py="4"
+            px="8"
+            ml="6"
+            maxW={400}
+            alignSelf="center"
+            color="gray.200"
+            position="relative"
+            bg="gray.800"
+            borderRadius="full"
+          >
+            <Input
+              px="4"
+              mx="4"
+              variant="unstyled"
+              placeholder="Buscar na plataforma"
+              color="gray.50"
+              _placeholder={{ color: "gray.400" }}
+            />
+            <Icon as={RiSearchLine} fontSize="20" />
+          </Flex>
+        )}
 
         <Flex align="center" ml="auto">
           <HStack
-            spacing="4"
-            mx="8"
-            pr="8"
+            spacing={["6", "8"]}
+            mx={["6", "8"]}
+            pr={["6", "8"]}
             py="1"
             color="gray.300"
             borderRightWidth={1}
@@ -68,14 +76,16 @@ export const Header = () => {
           </HStack>
 
           <Flex align="center">
-            <Box mr="4" textAlign="right">
-              <Text as="strong" display="block">
-                Marcos Renê
-              </Text>
-              <Text as="span" color="gray.300" fontSize="sm">
-                marcosrene789@gmail.com
-              </Text>
-            </Box>
+            {isWideVersion && (
+              <Box mr="4" textAlign="right">
+                <Text as="strong" display="block">
+                  Marcos Renê
+                </Text>
+                <Text as="span" color="gray.300" fontSize="sm">
+                  marcosrene789@gmail.com
+                </Text>
+              </Box>
+            )}
 
             <Avatar
               size="md"
