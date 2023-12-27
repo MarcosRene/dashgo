4f473b9d49d564a17deaaacef3ec96dfc6b24d4f
Marcos Renê <renemoura789@gmail.com>
2023-12-27 15:27:56 -0300

    feat: initial config miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import { SidebarDrawerProdiver } from "../contexts/SidebarDrawerContext";
 
+import { makeServer } from "../services/mirage";
+
 import { theme } from "../styles/theme";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
